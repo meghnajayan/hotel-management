4dbd2bf79ff2e53bd67866dd13fa6ff85d69c3ff
milena12sp <113570388+milena12sp@users.noreply.github.com>
2022-11-30 08:08:24 +0530

    Update Conn.java

--- src/Hotel/Management/System/Conn.java
+++ src/Hotel/Management/System/Conn.java
@@ -10,7 +10,7 @@ public class Conn {
     {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/hotelmanagmentsystem","root","0987654321vgt@#");
+            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/hotelmanagmentsystem","root","12345");
             statement = connection.createStatement();
         }catch(Exception e){
             e.printStackTrace();
